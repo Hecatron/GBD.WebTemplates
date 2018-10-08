1d22e1c3c99d7aa2858b0c35e87fcd67a51371bf
grbd <garlicbready@googlemail.com>
2018-10-08 15:27:29 +0100

    Added service example

--- src/ASPNet.Vue.BootStrap.v2/Startup.cs
+++ src/ASPNet.Vue.BootStrap.v2/Startup.cs
@@ -24,7 +24,12 @@ namespace ASPNet.Vue.BootStrap.v2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            // Add framework services.
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Simple example with dependency injection for a data provider.
+            services.AddSingleton<Providers.IWeatherProvider, Providers.WeatherProviderFake>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
