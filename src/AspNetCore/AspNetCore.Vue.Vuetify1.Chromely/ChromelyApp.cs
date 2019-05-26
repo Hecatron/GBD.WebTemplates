92d9de87fd4eb112ef388b248469870fdba0b76a
grbd <hecatrons.workshop@gmail.com>
2019-05-26 22:35:05 +0100

    more update

--- src/AspNetCore/AspNetCore.Vue.Vuetify1.Chromely/ChromelyApp.cs
+++ src/AspNetCore/AspNetCore.Vue.Vuetify1.Chromely/ChromelyApp.cs
@@ -9,43 +9,50 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace AspNetCore.Vue.Vuetify1.Chromely {
+
+    /// <summary> A chromely application wrapper class. </summary>
     public class ChromelyApp {
 
+        /// <summary> The starturl to point chromely towards. </summary>
+        /// <value> The start URL. </value>
         public string StartUrl { get; set; } = "local://app/chromely.html";
 
+        /// <summary> Chromely configuration. </summary>
+        /// <value> Chromely configuration. </value>
         public ChromelyConfiguration Config { get; set; }
 
         public IChromelyWindow Window { get; set; }
 
+        /// <summary> Setup the chromely instance. </summary>
+        /// <param name="args"> The arguments to pass in. </param>
         public void Setup(string[] args) {
             HostHelpers.SetupDefaultExceptionHandlers();
 
             var config = ChromelyConfiguration
-                .Create()
-                .WithHostMode(WindowState.Normal)
-                .WithHostTitle("chromely")
-                .WithHostIconFile("chromely.ico")
-                .WithAppArgs(args)
-                .WithHostSize(1200, 700)
-                .WithLogFile("logs\\chromely.cef_new.log")
-                .WithStartUrl(StartUrl)
-                .WithLogSeverity(LogSeverity.Info)
-                .WithDefaultSubprocess()
-                .UseDefaultLogger("logs\\chromely_new.log")
-                .UseDefaultResourceSchemeHandler("local", string.Empty)
-                .UseDefaultHttpSchemeHandler("http", "chromely.com")
+                    .Create()
+                    .WithHostMode(WindowState.Normal)
+                    .WithHostTitle("chromely")
+                    .WithHostIconFile("chromely.ico")
+                    .WithAppArgs(args)
+                    .WithHostSize(1200, 700)
+                    .WithLogFile("logs\\chromely.cef_new.log")
+                    .WithStartUrl(StartUrl)
+                    .WithLogSeverity(LogSeverity.Info)
+                    .WithDefaultSubprocess()
+                    .UseDefaultLogger("logs\\chromely_new.log")
+                    .UseDefaultResourceSchemeHandler("local", string.Empty)
+                    .UseDefaultHttpSchemeHandler("http", "chromely.com")
 
-                // The single process should only be used for debugging purpose.
-                // For production, this should not be needed when the app is published and an cefglue_winapi_netcoredemo.exe 
-                // is created.
+                    // The single process should only be used for debugging purpose.
+                    // For production, this should not be needed when the app is published and an cefglue_winapi_netcoredemo.exe 
+                    // is created.
 
-                // TODO Release check
-                // TODO add to wiki
-
-                // Alternate approach for multi-process, is to add a subprocess application
-                // .WithCustomSetting(CefSettingKeys.BrowserSubprocessPath, full_path_to_subprocess)
-                .WithCustomSetting(CefSettingKeys.SingleProcess, true);
-
+#if DEBUG
+                    // Alternate approach for multi-process, is to add a subprocess application
+                    // .WithCustomSetting(CefSettingKeys.BrowserSubprocessPath, full_path_to_subprocess)
+                    .WithCustomSetting(CefSettingKeys.SingleProcess, true)
+#endif
+                ;
 
             Window = ChromelyWindow.Create(config);
 
@@ -59,6 +66,9 @@ namespace AspNetCore.Vue.Vuetify1.Chromely {
             Window.ScanAssemblies();
         }
 
+        /// <summary> Starts Chromely using a async Task. </summary>
+        /// <param name="args"> The arguments to pass in. </param>
+        /// <returns> A Task. </returns>
         public Task Start(string[] args) {
             var tsk = new Task(() => {
                 Thread.Sleep(2000);
@@ -66,6 +76,5 @@ namespace AspNetCore.Vue.Vuetify1.Chromely {
             });
             return tsk;
         }
-
     }
 }
