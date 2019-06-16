eda215328a2a9c136c5fa9f23759addbc151e05a
grbd <hecatrons.workshop@gmail.com>
2019-06-16 20:34:59 +0100

    Added initial working vuetify2 beta template

---
+++ src/AspNetCore/AspNetCore.Vue.Vuetify2/ClientApp/common/routes.ts
@@ -0,0 +1,10 @@
+// Setup the routes for the different pages
+const routes = [
+  { path: "/", component: require("../components/pages/home.vue").default },
+  { path: "/test1", component: require("../components/pages/test1/test1.vue").default },
+  { path: "/test2", component: require("../components/pages/test2/test2.vue").default },
+  { path: "/counter", component: require("../components/pages/counter/counter.vue").default },
+  { path: "/fetchdata", component: require("../components/pages/fetchdata/fetchdata.vue").default },
+];
+
+export { routes };
